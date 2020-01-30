8ec8a46ac8d725ce51406e5f8eef1d95db7d57d4
relaxcore <yurii.rogulya@teamvoy.com>
2020-01-30 17:01:55 +0200

    add ability to update data file

--- README.md
+++ README.md
@@ -1,31 +1,42 @@
-# location ruby gem
+**GeoList** is a very simple ruby gem to get a list of all countries, cities and suburbs.
 
-**location** is a very simple ruby gem to get a list countries, cities and suburbs of the world.
-
-## Put this gem at your Gemfile:
+## Install
 ```ruby
-gem 'location', git: 'https://github.com/relaxcoredev/location.git', branch: 'develop'
+gem 'geolist', git: 'https://github.com/relaxcoredev/geolist.git', branch: 'develop'
 ```
 
-## List of countries:
+## Examples
+
+### List of countries:
 ```ruby
-Location.countries
+GeoList.countries
 # [{:name=>"Australia", :alpha2=>"AU", :alpha3=>"AUS"}, ...]
 ```
 
-## List of cities:
+### List of cities:
 ```ruby
-Location.cities('Australia')
+GeoList.cities('Australia')
 # => ["Adak", "Akhiok", "Akiachak", "Akiak", "Akutan", "Alakanuk", ...]
 ```
 
-## List of suburbs (only for Hong Kong):
+### List of suburbs (only for Hong Kong):
 ```ruby
-Location.suburbs('Hong Kong')
+GeoList.suburbs('Hong Kong')
 # ["Aberdeen", "Causeway Bay", "Central District","Cha Kwo Ling", "Chai Wan Kok", "Chek Chue", ...]
 ```
 
-## Simplified syntax with *get* method:
-* _Location.get_: list of countries (equivalent to `Location.countries`)
-* _Location.get(country)_: list of cities (equivalent to `Location.cities(country)`)
-* _Location.get(country, city)_: list of suburbs (equivalent to `Location.suburbs(city)`)
+### Simplified syntax with *get* method:
+* _GeoList.get_: list of countries (equivalent to `GeoList.countries`)
+* _GeoList.get(country)_: list of cities (equivalent to `GeoList.cities(country)`)
+* _GeoList.get(country, city)_: list of suburbs (equivalent to `GeoList.suburbs(city)`)
+
+### Geo data update
+
+```ruby
+GeoList.update_data
+
+# Will update data file to the latest version (using MaxMind free databases)
+# Can be added to migration or in some scheduler
+
+# Database is updated weekly, every Tuesday.
+```
--- lib/location.rb
+++ lib/location.rb
@@ -2,39 +2,67 @@
 
 require 'csv'
 require 'iso_country_codes'
+require 'open-uri'
+require 'zip'
+require 'pry'
 
-module Location
-  DATA_FILE = File.join(__dir__, 'data.csv')
+module GeoList
+  class << self
+    MAXMIND_URL    = 'https://download.maxmind.com/app/geoip_download'
+    MAXMIND_PARAMS = '?edition_id=GeoLite2-City-CSV&suffix=zip&license_key=PC4n5BvdsNQK3w6m'
+    MAXMIND_LINK   = MAXMIND_URL + MAXMIND_PARAMS
 
-  # position in CSV file
-  COUNTRY_NAME = 5
-  CITY         = 10
+    DATA_FILE = File.join(__dir__, 'data.csv')
 
-  def self.countries
-    ::IsoCountryCodes.all.map { |country| { name: country.name, alpha2: country.alpha2, alpha3: country.alpha3 } }
-  end
+    # position in CSV file
+    COUNTRY_NAME = 5
+    CITY         = 10
 
-  def self.cities(country_name)
-    return [] if country_name&.empty?
-    return ['Hong Kong'] if country_name == 'Hong Kong'
+    def update_data # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+      downloaded_file = open(MAXMIND_LINK) # rubocop:disable Security/Open
+      csv_filename    = 'GeoLite2-City-Locations-en.csv'
 
-    data.select { |row| row[COUNTRY_NAME] == country_name }.map { |row| row[CITY] }.uniq.compact.sort
-  end
+      Zip::File.open(downloaded_file) do |zip_file|
+        zip_file.each do |file|
+          next unless file.name.match?(/#{csv_filename}/)
 
-  def self.suburbs(city)
-    return [] unless city == 'Hong Kong'
+          path = File.join(__dir__, csv_filename)
+          zip_file.extract(file, path)
+        end
+      end
 
-    data.select { |row| row[COUNTRY_NAME] == city }.map { |row| row[CITY] }.uniq.compact.sort
-  end
+      FileUtils.rm(DATA_FILE) if File.exist?(DATA_FILE)
+      File.rename File.join(__dir__, csv_filename), File.join(__dir__, 'data.csv')
 
-  def self.get(country = nil, city = nil, _suburb = nil)
-    return countries       if country.nil?
-    return cities(country) if city.nil?
+      true
+    end
 
-    suburbs(city)
-  end
+    def countries
+      ::IsoCountryCodes.all.map { |country| { name: country.name, alpha2: country.alpha2, alpha3: country.alpha3 } }
+    end
 
-  def self.data
-    @data ||= CSV.read(DATA_FILE)
+    def cities(country_name)
+      return [] if country_name&.empty?
+      return ['Hong Kong'] if country_name == 'Hong Kong'
+
+      data.select { |row| row[COUNTRY_NAME] == country_name }.map { |row| row[CITY] }.uniq.compact.sort
+    end
+
+    def suburbs(city)
+      return [] unless city == 'Hong Kong'
+
+      data.select { |row| row[COUNTRY_NAME] == city }.map { |row| row[CITY] }.uniq.compact.sort
+    end
+
+    def get(country = nil, city = nil, _suburb = nil)
+      return countries       if country.nil?
+      return cities(country) if city.nil?
+
+      suburbs(city)
+    end
+
+    def data
+      @data ||= CSV.read(DATA_FILE)
+    end
   end
 end
